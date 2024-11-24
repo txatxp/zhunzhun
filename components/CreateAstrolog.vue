6228c59c349e5fe274eaa3700d7352f26ae09393
tangxingping <185389903@qq.com>
2024-11-24 17:43:13 +0800

    22

--- components/CreateAstrolog.vue
+++ components/CreateAstrolog.vue
@@ -171,9 +171,9 @@
 
             <div class="z-astro-title">线条类型：</div>
             <el-radio-group v-model="line">
-                <el-radio value="0" size="large">实线</el-radio>
+                <el-radio value="0" size="large">粗线</el-radio>
+                <el-radio value="2" size="large">细线</el-radio>
                 <el-radio value="1" size="large">虚线</el-radio>
-                
             </el-radio-group>
 
 
